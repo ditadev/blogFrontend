1568892560672b1129b9107fa528a83759772eb4
InyakaAbraham <abrahaminyaka@yahoo.com>
2023-01-31 20:30:31 +0100

    Refactored

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+const routes: Routes = [  
+  {path: '', component: LandingPageComponent, pathMatch: 'full' },
+  {path: 'login', component: SignInComponent },
+  {path: 'signup', component: SignUpComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoadingComponent,
+    LandingPageComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
