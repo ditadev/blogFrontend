8e7c90981b0b3cd1d1e972e703aad2574fb77910
InyakaAbraham <abrahaminyaka@yahoo.com>
2023-02-02 16:37:48 +0100

    Refactored

--- src/app/components/feeds/feeds.component.ts
+++ src/app/components/feeds/feeds.component.ts
@@ -1,32 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { ApiService } from 'src/app/services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feeds',
   templateUrl: './feeds.component.html',
   styleUrls: ['./feeds.component.css']
 })
-export class FeedsComponent   {
+export class FeedsComponent implements OnDestroy  {
   id!:number;
   name!:string;
+  subscription?: Subscription;
+
   constructor(
     private jwtHelper: JwtHelperService, 
     private apiService:ApiService
   ) {}
 
+  getUser():void{
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      this.id = this.jwtHelper.decodeToken(token).sub;
+    } else {
+      console.error("No token found in local storage.");
+    }    
+    this.subscription=this.apiService.getUser(this.id).subscribe(response=>{
+      this.name=response.data.username;
+      console.log(this.name);
+    });
+  }
+
   isUserAuthenticated() {
     const token = localStorage.getItem("jwt");
     if (token && !this.jwtHelper.isTokenExpired(token)) {
-      this.id=this.jwtHelper.decodeToken(token).sub;
-      this.apiService.getUser(this.id).subscribe(response=>{
-        this.name=response.data.username
-      });
-      console.log(name);
       return true;
     }
     else {
       return false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 }
