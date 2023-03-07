43992302744040f3a6da0c56575f31462fb3f111
ditadev <abrahaminyaka@yahoo.com>
2023-03-07 18:44:50 +0100

    Added download for articles

--- src/app/components/web/article/article.component.ts
+++ src/app/components/web/article/article.component.ts
@@ -68,21 +68,66 @@ export class ArticleComponent implements OnInit, OnDestroy {
   downloadPDF() {
     const element = document.getElementById('contentToConvert');
     if (element) {
-      html2canvas(element, { useCORS: true, allowTaint: true }).then(canvas => {
-        const contentWidth = canvas.width;
+      // Create a new PDF document
+      const pdf = new jsPDF('p', 'pt', 'a4');
+      const options = {
+        useCORS: true,
+        allowTaint: true,
+        scale: 1,
+        logging: true,
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+        scrollX: 0,
+        scrollY: -window.scrollY
+      };
+      // Convert the HTML content to a canvas using html2canvas
+      html2canvas(element, options).then((canvas) => {
+        // Get the total height of the content
         const contentHeight = canvas.height;
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const position = 0;
-        const imgData = canvas.toDataURL('image/png');
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = pdf.internal.pageSize.getHeight();
-        const ratio = contentHeight / contentWidth;
-        const height = pdfWidth * ratio;
-        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, height);
-        pdf.save('file.pdf');
+        // Set the page height to 950 pixels (adjust as needed)
+        const pageHeight = 1550;
+        // Calculate the number of pages needed to display the content
+        const pageCount = Math.ceil(contentHeight / pageHeight);
+        // Loop through each page and add it to the PDF document
+        for (let pageIndex = 0; pageIndex < pageCount; pageIndex++) {
+          // Calculate the y position of the page
+          const position = pageIndex * pageHeight * -1;
+          // Create a new canvas for the current page with padding at the top
+          const pageCanvas = document.createElement('canvas');
+          pageCanvas.width = canvas.width;
+          pageCanvas.height = pageHeight + 30; // add 30 pixels of padding at the top
+          const context = pageCanvas.getContext('2d');
+          if (context) {
+            // Fill the canvas with white color to create the padding at the top
+            context.fillStyle = '#fff';
+            context.fillRect(0, 0, pageCanvas.width, 30);
+            // Draw the current page onto the new canvas
+            context.drawImage(canvas, 0, position + 30); // add 30 pixels of padding at the top
+            // Add the canvas image to the PDF document
+            const imgData = pageCanvas.toDataURL('image/png');
+            const imgWidth = pdf.internal.pageSize.getWidth();
+            const imgHeight = (pageCanvas.height * imgWidth) / pageCanvas.width;
+            pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+            // Add a new page to the PDF document if there are more pages
+            if (pageIndex < pageCount - 1) {
+              // Add a new page to the PDF document with padding at the bottom
+              pdf.addPage();
+              pdf.setFillColor(255, 255, 255);
+              pdf.rect(0, pdf.internal.pageSize.getHeight() - 30, pdf.internal.pageSize.getWidth(), 30, 'F'); // add 30 pixels of padding at the bottom
+            }
+            // Save the PDF document when all pages have been added
+            if (pageIndex === pageCount - 1) {
+              pdf.save('file.pdf');
+            }
+          }
+        }
       });
     }
-  }  
+  }
+  
+  
+  
+  
 
   goBack():void{
     this.location.back();
